40ee12ba6b02b88291e3a4679c814aefdb0b4478
JohnnyWyles <97029546+JohnnyWyles@users.noreply.github.com>
2022-08-30 11:47:08 +0100

    Remove Comdex and all maturity exceptions

--- Params.py
+++ Params.py
@@ -3,7 +3,7 @@ incentivized_pool_ids = [
     197,
     461, 463, 481, 482, 497, 498,
     549, 553, 557, 571, 573, 577, 584, 585, 586,
-    600, 601, 602, 604, 605, 608, 611, 613, 619, 621, 625, 626, 627, 629, 637, 641, 644, 648, 651, 674, 678, 681, 690,
+    602, 604, 605, 608, 611, 613, 619, 621, 625, 626, 627, 629, 637, 641, 644, 648, 651, 674, 678, 681, 690,
     704, 712, 722, 725, 730, 731, 773
     ]
 
@@ -51,13 +51,10 @@ Minimums = {
 
 Maximums = {
     1 : 0.25,
-    600: 0,
-    601: 0
 }
 
 #Causes Maturity to be overruled, use for 1 proposal when changing minimums or matching incentives to ensure they are met
 MaturityExceptions = [
-    600, 601, 725
     ]
 
 match_limit = 0.30
